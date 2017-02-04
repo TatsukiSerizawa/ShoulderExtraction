b0f7e90699a1c06c01534d11198fe0a7ffff9e63
TatsukiSerizawa <iputatsuki@gmail.com>
2017-02-05 01:19:55 +0900

    肩周りだけトリミング

--- shoulder.py
+++ shoulder.py
@@ -7,37 +7,39 @@ import cv2
 
 #カスケード分類器のPATHを通して読み込む
 CLASSIFIER_PATH = '../haarcascades/haarcascade_frontalface_default.xml'
-shoulderCascade = cv2.CascadeClassifier(CLASSIFIER_PATH)
+faceCascade = cv2.CascadeClassifier(CLASSIFIER_PATH)
 
 #肩周りだけトリミングして輪郭線を取る関数
 def shoulderExtraction():
 
     #検出処理
-    shoulder = shoulderCascade.detectMultiScale(gray, 1.1, 3)
+    face = faceCascade.detectMultiScale(gray, 1.1, 3)
 
-    #顔の周りをトリミングして保存
-#    i = 0
-#    for rect in face:
-#      x = rect[0] * 0.5
-#      y = rect[1] * 0.05
-#      width  = rect[2] * 1.8
-#      height = rect[3] * 1.8
-#      dst = img[y:y+height, x:x+width]
-#      cv2.imwrite('image/face_img5.jpg', dst)
-#      cv2.imshow('face_img', dst)
-#      i += 1
+    #肩の周りをトリミングして保存
+    for rect in face:
+      x = 0
+      y = rect[2]
+      width  = img.shape[1]
+      height = rect[3] * 1.2
+      dst = img[y:y+height, x:x+width]
+      cv2.imwrite('image/shoulder_img5.jpg', dst)
+      cv2.imshow('shoulder_img5', dst)
 
     #キャニー法で２値化
-    canny = cv2.Canny(gray, 50,150)
-    cv2.imwrite('image/face_canny5.jpg', canny)
-    cv2.imshow('face_canny', canny)
+    canny = cv2.Canny(dst, 50,150)
+#    cv2.imwrite('image/shoulder_canny5.jpg', canny)
+#    cv2.imshow('shoulder_canny', canny)
 
     #輪郭抽出
     hoge, contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
-    cv2.imwrite('image/contours_img5.jpg', img)
-    cv2.imshow('contours_img5.jpg', img)
+    cv2.drawContours(dst, contours, -1, (0, 255, 0), 2)
+    cv2.imwrite('image/contours_img5.jpg', dst)
+    cv2.imshow('contours_img5.jpg', dst)
 
+    #輪郭線から最初に出てきた値を取る
+    for shWidth in dst:
+#        shoulderWidth = shWidth[1]
+        print(dst[shWidth])
 
 if __name__ == "__main__":
 
